d9fc563e0186e273e551a19492eefb564ce02f7d
ismael130308 <159185086+ismael130308@users.noreply.github.com>
2025-04-01 11:18:23 -0300

    01/04/2025

---
+++ flashcby-main/site-acessivel-projeto-inicial/sript.js
@@ -0,0 +1,20 @@
+document. addEventListener ('DOMContentLoaded', function() {
+    const aumentaFonteBotao = this. getElementByld('aumenta-fonte');
+    const diminuiFonteBotao = this. getElementById('diminuirfonte');
+    
+    let tamanhoAtualdaFonte = 1;
+    
+    aumentaFonteBotao.addEventListener('click',function () {
+    tamanhoAtualdaFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualdaFonte}rem`
+    
+    })
+    
+    diminuiFonteBotao.addEventListener('click',function () {
+    tamanhoAtualdaFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualdaFonte}rem`
+    
+    
+    })
+    
+    })
